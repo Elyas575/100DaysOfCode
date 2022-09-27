c820094eeaa1a7f41e39031c42e01f54011dfa92
unknown <Elyas575@hotmail.com>
2022-09-27 21:40:58 +0300

    day 8

--- JSPractice/sort-map-filter/index.js
+++ JSPractice/sort-map-filter/index.js
@@ -1,34 +1,4 @@
-const users = [
-  {
-    id:1,
-    name:"js 1",
-    isActive:true,
-    age:40
-  },
-  {
-    id:1,
-    name:"js 2",
-    isActive:true,
-    age:30
-  },
-  {
-    id:1,
-    name:"js 3",
-    isActive:false,
-    age:20
-  }
-]
-console.log(users)
 
+const multiply = (num1) => (num2) => num1*num2;
 
-
-
-
-let names = users.sort( (a,b) => (a.age<b.age ? -1 : 1))
-.filter( (user) => user.isActive )
-.map( (user) =>  user.age )
-
-
-
-
-console.log(names)
+console.log(multiply(3)(4))
